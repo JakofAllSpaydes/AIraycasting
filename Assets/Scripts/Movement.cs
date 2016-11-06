81d450dc05764b0c0eff50c8b1e72d50b88dff3b
Jack Guo <yiming7391@gmail.com>
2016-11-06 18:41:16 -0500

    Movement prototype - cat/mouse move ok.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	Rigidbody rbody;
+
+	public float moveSpeed;
+	public float randomizer;
+
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10f;
+		rbody = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		rbody.velocity = transform.forward * moveSpeed + Physics.gravity;
+
+		Ray moveRay = new Ray (transform.position, transform.forward);
+
+		Debug.DrawRay (transform.position, moveRay.direction*1.25f, Color.yellow);
+		if (Physics.SphereCast (moveRay, .5f, 1.25f)) {
+
+			randomizer = Random.Range(0f, 1f);
+
+			if (randomizer <= 0.5f){
+				
+				transform.Rotate (0f, 90f, 0f);
+
+			} else if(randomizer >0.5f && randomizer <= 1f){
+
+				transform.Rotate (0f, -90f, 0f);
+
+			}
+
+		}
+
+	}
+}
